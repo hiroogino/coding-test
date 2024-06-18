ec41fb6a3acd7c13a4ad3d6302dc8b0d9878ceaf
hirooogino <hirooogino@gmail.com>
2024-06-18 20:53:02 +0900

    add: tailwindcssのテストページを追加

---
+++ tailwindcss-test/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',  // src フォルダ内の全ての HTML および JS ファイルを対象にする
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
